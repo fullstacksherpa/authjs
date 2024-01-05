c151b8a7dbbe68bd5cf5e05aa07c29cd691410ed
fullstacksherpa <fullstacksherpa@gmail.com>
2024-01-04 21:25:02 -0800

    feat: add resend.com to our app and enable verification email send

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,8 @@ import * as z from "zod";
 import bcrypt from 'bcryptjs';
 import { db } from "@/lib/db";
 import { getUserByEmail } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -31,9 +33,12 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     }
   });
 
-  //todo: send verification token email
+  const verificationToken = await generateVerificationToken(email);
 
-  return { success: "User created!" };
+  //send verification token email
+  await sendVerificationEmail(verificationToken.email, verificationToken.token)
+
+  return { success: "Confirmation email send" };
 };
 
 //we are sending those message to ui to display form-error or form-success components
--- routes.ts
+++ routes.ts
@@ -8,6 +8,7 @@ export const publicRoutes = [
 export const authRoutes = [
     "/auth/login",
     "/auth/register",
+    "/auth/error",
 ]
 
 /**
